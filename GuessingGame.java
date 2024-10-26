93b75f1eadc732da4b499a574b08cb00c9244796
Antbjo2001 <abjorquist@gmail.com>
2024-10-26 22:36:11 +0200

    Task 3 next time

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
+
 public class GuessingGame {
     
     public static void main(String[] args) {
 
         Guesser guessNumber = new Guesser(0, 1000);
+        Scanner scanner = new Scanner(System.in);
 
 guessNumber.start();
 
+        scanner.getReply();
     }
 }
